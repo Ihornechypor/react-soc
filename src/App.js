8ddf26e382fb3613a2d9e25578247f7dd77da492
ihor.nechypor <ihor.nechypor@plej.pl>
2021-03-23 20:25:53 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./layouts/Header/Header";
 import Nav from "./layouts/Nav/Nav";
 import Main from "./layouts/Main/Main";
 import Profile from "./pages/Profile/Profile";
+import Messages from "./pages/Messages/Messages";
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Header />
       <Nav />
       <Main>
-        <Profile />
+        {/* <Profile /> */}
+        <Messages />
       </Main>
     </div>
   );
---
+++ src/pages/Messages/Messages.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Messages.module.css";
+
+const Messages = () => {
+  return <>some</>;
+};
+
+export default Messages;
